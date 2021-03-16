d71fccdb25c5953a9faeb1002203ef66193dce8c
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-03-17 00:07:27 +0200

    created route to send a string

---
+++ myApp.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+console.log("Hello World" );
+
+app.get('/', (req, res) => {
+    res.send('Response String');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
