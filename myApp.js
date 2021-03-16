208712775c8f8ca7c6f84d8de7d84e71c0412dfc
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-03-17 00:39:14 +0200

    serve json on a specific route

--- myApp.js
+++ myApp.js
@@ -5,13 +5,17 @@ console.log("Hello World" );
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
-});
+});*/
 
 /*app.get('/', (req, res) => {
     res.send('Response String');
 });*/
+
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"});
+});
 
 
 
