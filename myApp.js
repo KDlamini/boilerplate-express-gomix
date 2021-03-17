ae6640ac6bd033c63a576d2e3ee32cbb1de392d3
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-03-17 11:08:56 +0200

    added an env file to use in the router

--- myApp.js
+++ myApp.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 console.log("Hello World" );
 
 app.use('/public', express.static(__dirname + '/public'));
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
-});*/
+});
 
 /*app.get('/', (req, res) => {
     res.send('Response String');
 });*/
 
-app.get('/json', (req, res) => {
+/*app.get('/json', (req, res) => {
     res.json({"message": "Hello json"});
+});*/
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
 });
 
 
